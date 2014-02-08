f58329e4fbf7bc50229e03a508d6a6cd3b6c7645
DynnaFloM <kjetil@docstream.no>
2014-02-08 08:18:35 +0000

    Autotools again

--- src/parser.c
+++ src/parser.c
@@ -18,6 +18,10 @@ startElement(void *userData, const char *name, const char **atts)
     printf("startElem:");
     printf(name );
     printf( "\n" );
+
+    // if opf:meta and cnt_attrs=2
+    //
+    // if cnt_attrs=1
 }
 
 
