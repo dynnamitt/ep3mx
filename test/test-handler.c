6cf8180d13d921e5d914ac32d4a92d6d38def990
DynnaFloM <kjetil@docstream.no>
2014-02-10 18:20:51 +0000

    ok smooth

---
+++ test/test-handler.c
@@ -0,0 +1,13 @@
+#include "include/parser.h"
+
+void
+startElement(void *userData, const char *name, const char **atts)
+{
+    printf("startElem:");
+    printf(name );
+    printf( "\n" );
+
+    // if opf:meta and cnt_attrs=2
+    //
+    // if cnt_attrs=1
+}
